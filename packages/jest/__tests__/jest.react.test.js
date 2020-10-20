04edd09c1c5c2951a2413228ef218a466e87ec92
Evan.Burbidge <burbidge@tcd.ie>
2020-10-20 20:27:26 +0100

    update to use react and vue

---
+++ packages/jest/__tests__/jest.react.test.js
@@ -0,0 +1,14 @@
+"use strict";
+const React = require("react");
+const ReactDOMServer = require("react-dom/server");
+
+const runA11yJest = require("..");
+
+describe("jest", () => {
+  test("it should call the runA11yMethod", async () => {
+    const element = React.createElement("img", { src: "#" });
+    const html = ReactDOMServer.renderToString(element);
+    const results = await runA11yJest(html)
+    expect(results.violations.length).toBeGreaterThan(1);
+  });
+});
---
+++ packages/jest/__tests__/jest.vue.test.js
@@ -0,0 +1,23 @@
+const runA11yJest = require("..");
+
+const Image = {
+  data: () => ({ src: '#' }),
+  template: '<img id="test-image" :src="src" />'
+}
+
+describe('Vue', () => {
+  it('renders correctly', async () => {
+    const { mount } = require('@vue/test-utils')
+    const wrapper = mount(Image)
+    const results = await runA11yJest(wrapper.element)
+    expect(results.violations.length).toBeGreaterThan(1);
+  })
+
+  it('renders a vue testing library container correctly', async () => {
+    const { render } = require('@testing-library/vue')
+    const { container } = render(Image)
+    const results = await runA11yJest(container)
+    expect(results.violations.length).toEqual(1);
+  })
+
+})
