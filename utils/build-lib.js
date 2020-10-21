c3a9b35987857d0288707b9b5b79b23ec3872d47
Evan.Burbidge <burbidge@tcd.ie>
2020-10-21 18:38:53 +0100

    get builds working

---
+++ utils/build-lib.js
@@ -0,0 +1,64 @@
+const chalk = require('chalk')
+const babel = require('@babel/core')
+const { promisify } = require('util')
+const glob = promisify(require('glob'))
+const mkdirp = promisify(require('mkdirp'))
+const fs = require('fs')
+const path = require('path')
+
+const transformFile = promisify(babel.transformFile)
+const writeFile = promisify(fs.writeFile)
+const stat = promisify(fs.stat)
+
+const SOURCE = 'packages/*/lib/*.js'
+// Files not to build (such as tests)
+const IGNORE = /\.__test__\.js$|__mocks__|companion\//
+// Files that should trigger a rebuild of everything on change
+const META_FILES = [
+  'babel.config.js',
+  'package.json',
+  'package-lock.json',
+  'utils/build-lib.js'
+]
+
+function lastModified (file) {
+  return stat(file).then((s) => s.mtime)
+}
+
+async function buildLib () {
+  const metaMtimes = await Promise.all(META_FILES.map((filename) =>
+    lastModified(path.join(__dirname, '..', filename))
+  ))
+  const metaMtime = Math.max(...metaMtimes)
+
+  const files = await glob(SOURCE)
+  for (const file of files) {
+    if (IGNORE.test(file)) continue
+    const libFile = file.replace('/lib/', '/dist/')
+
+    // on a fresh build, rebuild everything.
+    if (!process.env.FRESH) {
+      const srcMtime = await lastModified(file)
+      const libMtime = await lastModified(libFile)
+        .catch(() => 0) // probably doesn't exist
+      // Skip files that haven't changed
+      if (srcMtime < libMtime && metaMtime < libMtime) {
+        continue
+      }
+    }
+
+    const { code, map } = await transformFile(file, { sourceMaps: true })
+    mkdirp.sync(path.dirname(libFile))
+    await Promise.all([
+      writeFile(libFile, code),
+      writeFile(libFile + '.map', JSON.stringify(map))
+    ])
+    console.log(chalk.green('Compiled lib:'), chalk.magenta(libFile))
+  }
+}
+
+console.log('Using Babel version:', require('@babel/core/package.json').version)
+buildLib().catch((err) => {
+  console.error(err.stack)
+  process.exit(1)
+})
