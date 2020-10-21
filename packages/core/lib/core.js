3f9f87e5568a80f4acbe6f8d6d4ddda14f6f7edd
Evan.Burbidge <burbidge@tcd.ie>
2020-10-21 19:14:41 +0100

    make firebase configurable

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,42 +1,42 @@
 "use strict";
-
 const admin = require("firebase-admin");
-const { normaliseBuild } = require('@a11ycore/utils');
-const serviceKey = require("./serviceAccount.json");
+const { normaliseBuild } = require("@a11ycore/utils");
 
 module.exports = {
-  sendBuild,
+  configureFirebase,
 };
 
-function sendBuild(projectId, results) {
+function configureFirebase(serviceKey, databaseUrl, firebaseConfig = {}) {
   const defaultApp = admin.initializeApp({
+    databaseURL: databaseUrl,
     credential: admin.credential.cert(serviceKey),
-    databaseURL: "https://a11ycore.firebaseio.com",
   });
   const db = defaultApp.firestore();
   // creating a starting path in our database
-  const builds = db.collection("builds");
-  const projects = db.collection("projects");
-  return new Promise((resolve, reject) => {
-    if (!projectId) {
-      throw Error("you must supply a project id");
-    }
-    const normalizedResults = normaliseBuild(projectId, results, admin);
-    projects
-      .doc(projectId)
-      .get()
-      .then((document) => {
-        if (!document.empty) {
-          builds.add(normalizedResults).then(resolve).catch(reject);
-        } else {
+  const builds = db.collection(firebaseConfig.buildsDb);
+  const projects = db.collection(firebaseConfig.projectsDb);
+  return (projectId, results) => {
+    return new Promise((resolve, reject) => {
+      if (!projectId) {
+        throw Error("you must supply a project id");
+      }
+      const normalizedResults = normaliseBuild(projectId, results, admin);
+      projects
+        .doc(projectId)
+        .get()
+        .then((document) => {
+          if (!document.empty) {
+            builds.add(normalizedResults).then(resolve).catch(reject);
+          } else {
+            reject();
+            throw Error("that project does not exist");
+          }
+        })
+        .catch((error) => {
           reject();
+          console.error(error);
           throw Error("that project does not exist");
-        }
-      })
-      .catch((error) => {
-        reject();
-        console.error(error);
-        throw Error("that project does not exist");
-      });
-  });
+        });
+    });
+  };
 }
