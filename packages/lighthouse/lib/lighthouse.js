53562d037ef10721940a9a68bdcb7e59e4fd8caf
eburbidge <eburbidge@hubspot.com>
2020-10-22 14:33:32 +0100

    updating lighthouse

--- packages/lighthouse/lib/lighthouse.js
+++ packages/lighthouse/lib/lighthouse.js
@@ -9,13 +9,25 @@ module.exports = {
     runA11yLighthouse,
 };
 
+const lighthouseOptions = {
+    extends: 'lighthouse:default',
+    logLevel: 'info',
+    output: 'json',
+    disableDeviceEmulation: true,
+    onlyCategories: ['accessibility'],
+    settings: {
+      onlyCategories: ['accessibility'],
+      output: ['json'],
+    },
+  }
+
 function runA11yLighthouse(url = 'https://www.thewebuiguy.com/', chromeFlags = ['--headless'], options = {}) {
     return new Promise((resolve, reject) => {
         chromeLauncher.launch({
             chromeFlags,
         })
         .then(chrome => {
-            const opts = merge({}, options, {device: 'desktop', output: 'json',  onlyCategories: [], port: chrome.port});
+            const opts = merge({}, options, lighthouseOptions, {port: chrome.port});
             lighthouse(url, opts)
                 .then(results => {
                     resolve(results);
@@ -31,7 +43,7 @@ runA11yLighthouse()
     .then(results => {
         try {
             const report = JSON.parse(results.report);
-            console.log(report);
+            console.log(report.audits);
         } catch (e) {
             console.log(e);
         }
