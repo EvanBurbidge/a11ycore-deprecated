66a23b07a6af1176c0a324bbff565a282e4c2b0a
eburbidge <eburbidge@hubspot.com>
2020-10-22 13:43:13 +0100

    update reporter test

--- packages/reporter/__tests__/reporter.test.js
+++ packages/reporter/__tests__/reporter.test.js
@@ -1,6 +1,21 @@
 'use strict';
 
-const reporter = require('..');
+const { reporter } = require('..');
+const { runA11yJest } = require('@a11ycore/jest');
+const React = require("react");
+const ReactDOMServer = require("react-dom/server");
+
+describe("jest", () => {
+  test("it should call the runA11yMethod", async () => {
+    const element = React.createElement("img", { src: "#" });
+    const html = ReactDOMServer.renderToString(element);
+    const results = await runA11yJest(html)
+    expect(results.violations.length).toBeGreaterThan(1);
+    reporter('','',results);
+  });
+});
+
+
 
 describe('reporter', () => {
     it('needs tests');
