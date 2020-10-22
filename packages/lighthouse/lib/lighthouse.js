39e9f3c42bc801065f9ea1740c480265eb3ba7a3
eburbidge <eburbidge@hubspot.com>
2020-10-22 14:05:34 +0100

    update lighthouse package

--- packages/lighthouse/lib/lighthouse.js
+++ packages/lighthouse/lib/lighthouse.js
@@ -1,7 +1,32 @@
 'use strict';
+const fs = require('fs');
+const merge = require('lodash.merge');
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
 
-module.exports = lighthouse;
 
-function lighthouse() {
-    // TODO
+module.exports = {
+    runA11yLighthouse,
+};
+
+function runLighthouse (chrome, options) {
+    
+}
+
+function runA11yLighthouse(chromeFlags = [], options = {}) {
+    return new Promise((resolve, reject) => {
+        chromeLauncher.launch({
+            chromeFlags,
+        })
+        .then(chrome => {
+            const opts = merge({}, options, {logLevel: 'info', output: 'json', onlyCategories: ['accesssibility'], port: chrome.port});
+            lighthouse('http://www.google.com/ncr', opts)
+                .then(results => {
+                    resolve(results);
+                    chrome.kill();
+                })
+                .catch(err => reject(err));
+        })
+        .catch(err => reject(err));
+    });
 }
