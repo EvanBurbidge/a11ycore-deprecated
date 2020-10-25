f16cf3cf5ddf92430ca42a77b234408ee146024f
Evan.Burbidge <burbidge@tcd.ie>
2020-10-25 17:54:46 +0000

    update reporter test

--- packages/reporter/__tests__/reporter.test.js
+++ packages/reporter/__tests__/reporter.test.js
@@ -2,6 +2,7 @@
 
 const { reporter } = require('..');
 const { runA11yJest } = require('@a11ycore/jest');
+const { normaliseBuild } = require('@a11ycore/utils');
 const React = require("react");
 const ReactDOMServer = require("react-dom/server");
 
@@ -11,12 +12,8 @@ describe("jest", () => {
     const html = ReactDOMServer.renderToString(element);
     const results = await runA11yJest(html)
     expect(results.violations.length).toBeGreaterThan(1);
-    reporter('','',results);
+    const formattedResults = normaliseBuild(results);
+    console.log(formattedResults)
+    await reporter('wsR4nYaVr8uAbW99E38Y', '82ba3e42-747b-463f-b21e-b55d21e44ebd', formattedResults);
   });
-});
-
-
-
-describe('reporter', () => {
-    it('needs tests');
 });
